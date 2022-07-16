b019751a9bb0ff7fbe81eb0e70d5b81c72fdad9a
Marko Wong <marko.wong@student.unsw.edu.au>
2022-07-16 23:52:01 +1000

    changed internal struct to use mulitset instead of vector due to complexity requirements and wrote some unit tests

--- include/gdwg/graph.hpp
+++ include/gdwg/graph.hpp
@@ -2,6 +2,7 @@
 #define GDWG_GRAPH_HPP
 #include <map>
 #include <vector>
+#include <set>
 #include <algorithm>
 #include <memory>
 #include <iostream>
@@ -12,38 +13,38 @@ namespace gdwg {
 		public:
 			// Constructor
 			graph() noexcept {
-				nodes_ = std::make_unique<std::map<N, std::vector<std::pair<N,E>>>>();
+				nodes_ = std::make_unique<std::map<N, std::multiset <std::pair<N,E>>>>();
 			}
 			graph(std::initializer_list<N> list){
-				nodes_ = std::make_unique<std::map<N, std::vector<std::pair<N,E>>>>();
+				nodes_ = std::make_unique<std::map<N, std::multiset <std::pair<N,E>>>>();
 				for (auto itr = list.begin(); itr != list.end(); ++itr) {
-					nodes_.get()->insert({*itr, std::vector<std::pair<N,E>>{}});
+					nodes_.get()->insert({*itr, std::multiset <std::pair<N,E>>{}});
 				}
 			}
 			template<typename InputIt>
 			graph(InputIt first, InputIt last){
-				nodes_ = std::make_unique<std::map<N, std::vector<std::pair<N,E>>>>();
+				nodes_ = std::make_unique<std::map<N, std::multiset <std::pair<N,E>>>>();
 				for (auto itr = first; itr != last; ++itr) {
-					nodes_.get()->insert({*itr, std::vector<std::pair<N,E>>{}});
+					nodes_.get()->insert({*itr, std::multiset <std::pair<N,E>>{}});
 				}
 
 
 			}
-			graph(graph&& other) noexcept: nodes_{std::exchange(other.nodes_, nullptr)}{};
+			graph(graph&& other) noexcept: nodes_{std::exchange(other.nodes_, graph().nodes_)}{};
 
 			auto operator=(graph&& other) noexcept -> graph&{
-				nodes_ = std::exchange(other.nodes_, nullptr);
+				nodes_ = std::exchange(other.nodes_, graph().nodes_);
 				return *this;
 			}
 
 			graph(graph const& other) noexcept{
-				nodes_ = std::make_unique<std::map<N, std::vector<std::pair<N,E>>>>();
+				nodes_ = std::make_unique<std::map<N, std::multiset <std::pair<N,E>>>>();
 				for (auto it = other.nodes_.get()->begin(); it != other.nodes_.get()->end(); ++it) {
 					nodes_.get()->insert({it->first, it->second});
 				}
 			}
 			auto operator=(graph const& other) -> graph&{
-				nodes_ = std::make_unique<std::map<N, std::vector<std::pair<N,E>>>>();
+				nodes_ = std::make_unique<std::map<N, std::multiset <std::pair<N,E>>>>();
 				for (auto it = other.nodes_.get()->begin(); it != other.nodes_.get()->end(); ++it) {
 					nodes_.get()->insert({it->first, it->second});
 				}
@@ -54,7 +55,7 @@ namespace gdwg {
 			auto insert_node(N const& value) -> bool{
 				if (!is_node(value)) {
 				// not found
-					nodes_.get()->insert({value, std::vector<std::pair<N,E>>{}});
+					nodes_.get()->insert({value, std::multiset <std::pair<N,E>>{}});
 					return true;
 				}
 				// found
@@ -63,9 +64,9 @@ namespace gdwg {
 
 			auto insert_edge(N const& src, N const& dst, E const& weight) -> bool{
 				if (is_node(src) && is_node(dst)) {
-					auto edge_vec = nodes_.get()->at(src);
-					if (std::find(edge_vec.begin(), edge_vec.end(), std::make_pair(dst, weight)) == edge_vec.end()){
-						nodes_.get()->at(src).push_back(std::make_pair(dst, weight));
+					auto edge_set = nodes_.get()->at(src);
+					if (std::find(edge_set.begin(), edge_set.end(), std::make_pair(dst, weight)) == edge_set.end()){
+						nodes_.get()->at(src).insert(std::make_pair(dst, weight));
 						return true;
 					}
 					return false;
@@ -94,19 +95,19 @@ namespace gdwg {
 				}
 				// For all outgoing edges
 				for (auto& [dst, weight] : nodes_.get()->at(old_data)){
-					nodes_.get()->at(new_data).push_back(std::make_pair(dst, weight));
+					nodes_.get()->at(new_data).insert(std::make_pair(dst, weight));
 				}
 				// remove old node
 				nodes_.get()->erase(old_data);
 				// For all nodes replace outgoing edges with old_node
 				for (auto it = nodes_.get()->begin(); it != nodes_.get()->end(); ++it) {
-					auto edge_vec = nodes_.get()->at(it->first);
-					std::transform(edge_vec.begin(), edge_vec.end(), nodes_.get()->at(it->first).begin(), [old_data, new_data](std::pair<N,E> &edge) {
-						if (edge.first == old_data){
-							return(std::make_pair(new_data, edge.second));
+					auto edge_set = nodes_.get()->at(it->first);
+					for (auto& [dst,weight] : edge_set){
+						if (dst == old_data){
+							edge_set.erase(std::make_pair(dst, weight));
+							edge_set.insert(std::make_pair(new_data, weight));
 						}
-						return edge;
-					});
+					}
 				}
 			}
 			auto erase_node(N const& value) -> bool{
@@ -119,8 +120,8 @@ namespace gdwg {
 					return false;
 				}
 				try{
-					auto edge_vec = nodes_.get()->at(src);
-					edge_vec.erase(std::remove(edge_vec.begin(), edge_vec.end(), std::make_pair(dst, weight)), edge_vec.end());
+					auto edge_set = nodes_.get()->at(src);
+					edge_set.erase(std::remove(edge_set.begin(), edge_set.end(), std::make_pair(dst, weight)), edge_set.end());
 					return true;
 				}
 				catch(...){
@@ -155,13 +156,12 @@ namespace gdwg {
 			}
 			[[nodiscard]] auto nodes() -> std::vector<N>{
 				std::vector<N> vec;
-				std::transform( nodes_.get()->begin(), nodes_.get()->end(), std::back_inserter(vec), first(nodes_.get()));
-				std::sort(vec.begin(), vec.end());
+				std::transform( nodes_.get()->begin(), nodes_.get()->end(), std::back_inserter(vec), [](auto &map){ return map.first;});
 				return vec;
 			}
 			[[nodiscard]] auto weights(N const& src, N const& dst) -> std::vector<E>{
 				if (!(is_node(src) && is_node(dst))){
-					throw std::runtime_error("Cannot call gdwg::graph<N, E>::merge_replace_node on old or new data if they don't exist in the graph");
+					throw std::runtime_error("Cannot call gdwg::graph<N, E>::weights if src or dst node don't exist in the graph");
 				}
 				std::vector<E> vec;
 				for (auto& [edg_dst, weight] : nodes_.get()->at(src)){
@@ -169,18 +169,17 @@ namespace gdwg {
 						vec.push_back(weight);
 					}
 				}
-				std::sort(vec.begin(), vec.end());
 				return vec;
 			}
 			// [[nodiscard]] auto find(N const& src, N const& dst, E const& weight) -> iterator;
 			[[nodiscard]] auto connections(N const& src) -> std::vector<N>{
 				if (!is_node(src)){
-					throw std::runtime_error("Cannot call gdwg::graph<N, E>::merge_replace_node on old or new data if they don't exist in the graph");
+					throw std::runtime_error("Cannot call gdwg::graph<N, E>::connections if src doesn't exist in the graph");
 				}
-				std::vector<E> vec;
-				auto edge_vec = nodes_.get()->at(src);
-				std::transform(edge_vec.begin(), edge_vec.end(), std::back_inserter(vec), first(nodes_.get()->at(src)));
-				std::sort(vec.begin(), vec.end());
+				std::vector<N> vec;
+				auto edge_set = nodes_.get()->at(src);
+				// std::transform(edge_set.begin(), edge_set.end(), std::back_inserter(vec), first(nodes_.get()->at(src)));
+				// std::sort(vec.begin(), vec.end());
 				return vec;
 			}
 
@@ -189,8 +188,8 @@ namespace gdwg {
 			// [[nodiscard]] auto end() const -> iterator;
 
 			// Comparisons
-			[[nodiscard]] auto operator==(graph const& other) -> bool{
-				return (nodes_.get() == other.nodes_.get());
+			[[nodiscard]] auto operator==(graph const& other) const-> bool{
+				return (this->nodes_.get() == other.nodes_.get());
 			}
 
 			// Extractor
@@ -222,7 +221,7 @@ namespace gdwg {
 			// auto operator==(iterator const& other) -> bool;
 
 		private:
-			std::unique_ptr<std::map<N, std::vector<std::pair<N,E>>>> nodes_;
+			std::unique_ptr<std::map<N, std::multiset<std::pair<N,E>>>> nodes_;
 	};
 
 } // namespace gdwg
--- test/graph/graph_test1.cpp
+++ test/graph/graph_test1.cpp
@@ -1,6 +1,8 @@
 #include "gdwg/graph.hpp"
 
 #include <catch2/catch.hpp>
+#include <vector>
+#include <list>
 
 TEST_CASE("basic test") {
 	// This will not compile straight away
@@ -10,15 +12,183 @@ TEST_CASE("basic test") {
 	CHECK(g.is_node(n));
 }
 TEST_CASE("Constructor Unit Tests") {
-	SECTION(){}
+	SECTION("default"){
+		CHECK_NOTHROW(gdwg::graph<int, std::string>());
+		auto g1 = gdwg::graph<int, std::string>();
+		CHECK(g1.empty() == true);
+
+		CHECK_NOTHROW(gdwg::graph<int, int>());
+		auto g2 = gdwg::graph<int, int>();
+		CHECK(g2.empty() == true);
+	}
+	SECTION("initialiser list"){
+		auto g1 = gdwg::graph<std::string, int>{"A", "B", "Marko"};
+		auto vec1 = g1.nodes();
+		CHECK(vec1.size() == 3);
+		CHECK(g1.is_node("A") == true);
+		CHECK(g1.is_node("B") == true);
+		CHECK(g1.is_node("Marko") == true);
+
+		auto g2 = gdwg::graph<int, std::string>{-1234,-3,0,420};
+		auto vec2 = g2.nodes();
+		CHECK(vec2.size() == 4);
+		CHECK(g2.is_node(-1234) == true);
+		CHECK(g2.is_node(-3) == true);
+		CHECK(g2.is_node(0) == true);
+		CHECK(g2.is_node(420) == true);
+	}
+	SECTION("interator"){
+		auto in_vec1 = std::vector<std::string>{"A", "B", "Marko"};
+		auto g1 = gdwg::graph<std::string, int>(in_vec1.begin(), in_vec1.end());
+		auto vec1 = g1.nodes();
+		CHECK(vec1.size() == 3);
+		CHECK(g1.is_node("A") == true);
+		CHECK(g1.is_node("B") == true);
+		CHECK(g1.is_node("Marko") == true);
+
+		auto in_list1 = std::list<int>{-1234,-3,0,420};
+		auto g2 = gdwg::graph<int, std::string>(in_list1.begin(), in_list1.end());
+		auto vec2 = g2.nodes();
+		CHECK(vec2.size() == 4);
+		CHECK(g2.is_node(-1234) == true);
+		CHECK(g2.is_node(-3) == true);
+		CHECK(g2.is_node(0) == true);
+		CHECK(g2.is_node(420) == true);
+	}
+	SECTION("move"){
+		auto in_vec1 = std::vector<std::string>{"A", "B", "Marko"};
+		auto g1 = gdwg::graph<std::string, int>(in_vec1.begin(), in_vec1.end());
+		auto vec1 = g1.nodes();
+
+		auto g2 = gdwg::graph<std::string, int>(std::move(g1));
+		auto vec2 = g2.nodes();
+		CHECK(vec2.size() == 3);
+		CHECK(g2.is_node("A") == true);
+		CHECK(g2.is_node("B") == true);
+		CHECK(g2.is_node("Marko") == true);
+		CHECK(g1.empty() == true);
+
+		auto g3 = std::move(g2);
+		auto vec3 = g3.nodes();
+		CHECK(vec3.size() == 3);
+		CHECK(g3.is_node("A") == true);
+		CHECK(g3.is_node("B") == true);
+		CHECK(g3.is_node("Marko") == true);
+		CHECK(g2.empty() == true);
+	}
+	SECTION("copy"){
+		auto in_vec1 = std::vector<std::string>{"A", "B", "Marko"};
+		auto g1 = gdwg::graph<std::string, int>(in_vec1.begin(), in_vec1.end());
+		auto vec1 = g1.nodes();
+		CHECK(vec1.size() == 3);
+		CHECK(g1.is_node("A") == true);
+		CHECK(g1.is_node("B") == true);
+		CHECK(g1.is_node("Marko") == true);
+
+		auto g2 = gdwg::graph<std::string, int>(g1);
+		CHECK(g1 == g2);
+
+		auto g3 = g2;
+		CHECK(g3 == g2);
+	}
 }
 
 TEST_CASE("Accessor Unit Tests") {
+	SECTION("is_node"){
+		auto g1 = gdwg::graph<std::string, int>{"A", "B", "Marko"};
+		CHECK(g1.is_node("A") == true);
+		CHECK(g1.is_node("B") == true);
+		CHECK(g1.is_node("Marko") == true);
+		CHECK(g1.is_node("marko") == false);
+	}
+	SECTION("empty"){
+		auto g1 = gdwg::graph<std::string, int>();
+		CHECK(g1.empty() == true);
+		auto g2 = gdwg::graph<std::string, int>{"2"};
+		CHECK(g2.empty() == false);
+	}
+	SECTION("is_connected"){
+		auto g1 = gdwg::graph<std::string, int>{"A", "B", "Marko"};
+		g1.insert_edge("A", "B", 5);
+		CHECK(g1.is_connected("A", "B") == true);
+		CHECK(g1.is_connected("B", "A") == false);
+
+		CHECK_THROWS_AS(g1.is_connected("C", "A"), std::runtime_error);
+		CHECK_THROWS_WITH(g1.is_connected("C", "A"), "Cannot call gdwg::graph<N, E>::merge_replace_node on old or new data if they don't exist in the graph");
+
+		CHECK_THROWS_AS(g1.is_connected("A", "D"), std::runtime_error);
+		CHECK_THROWS_WITH(g1.is_connected("A", "D"), "Cannot call gdwg::graph<N, E>::merge_replace_node on old or new data if they don't exist in the graph");
+	}
+	SECTION("nodes"){
+		auto g1 = gdwg::graph<std::string, int>{"B", "A", "Marko"};
+		auto src_vec = g1.nodes();
+		CHECK(src_vec == std::vector<std::string>{"A", "B", "Marko"});
+	}
+	SECTION("weights"){
+		auto g = gdwg::graph<std::string, int>{};
+		g.insert_node("hello");
+		g.insert_node("how");
+		g.insert_node("are");
+
+		g.insert_edge("hello", "how", 5);
+		g.insert_edge("hello", "are", 8);
+		g.insert_edge("hello", "are", 2);
+		auto src_weights = g.weights("hello", "are");
+		CHECK(src_weights == std::vector<int>{2,8});
+
+		CHECK_THROWS_AS(g.weights("C", "are"), std::runtime_error);
+		CHECK_THROWS_WITH(g.weights("C", "are"), "Cannot call gdwg::graph<N, E>::weights if src or dst node don't exist in the graph");
+		CHECK_THROWS_AS(g.weights("are", "C"), std::runtime_error);
+		CHECK_THROWS_WITH(g.weights("are", "C"), "Cannot call gdwg::graph<N, E>::weights if src or dst node don't exist in the graph");
+	}
+	SECTION("connections"){
+		auto g = gdwg::graph<std::string, int>{};
+		g.insert_node("hello");
+		g.insert_node("how");
+		g.insert_node("are");
+		g.insert_node("you?");
+
+		g.insert_edge("hello", "are", 8);
+		g.insert_edge("hello", "are", 2);
+		g.insert_edge("how", "you?", 1);
+		g.insert_edge("how", "hello", 4);
+		g.insert_edge("are", "you?", 3);
+
+		auto src_edges = g.connections("hello");
+		CHECK(src_edges == std::vector<std::string>{"are", "are", "how"});
+	}
 }
 TEST_CASE("Modifier Unit Tests") {
+	SECTION("insert_node"){
+
+	}
+	SECTION("insert_edge"){
+
+	}
+	SECTION("replace_node"){
+
+	}
+	SECTION("merge_replace_node"){
+
+	}
+	SECTION("erase_node"){
+
+	}
+	SECTION("erase_edge"){
+
+	}
+	SECTION("clear"){
+
+	}
 }
 
-TEST_CASE("Operatir Unit Tests") {
+TEST_CASE("Operator Unit Tests") {
+	SECTION("=="){
+
+	}
+	SECTION("<<"){
+
+	}
 }
 TEST_CASE("Iterator Unit Tests") {
 }
